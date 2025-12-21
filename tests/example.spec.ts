af7db00d93111bedcf63da1f2176b5363eee0cf9
Juanes <juanes.lopera326@gmail.com>
2025-12-21 18:55:27 -0500

    Fix test annotations by removing brackets from tags in example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 //aqui empieza el test con jenkins
-test('[@smoke] has title', async ({ page }) => {
+test('@smoke has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('[@regression] get started link', async ({ page }) => {
+test('@regression get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
